a094f39ab6bb30704dbb8b311266afb5a3ce96f4
Asees <asees1999@gmail.com>
2023-12-25 23:40:14 +0530

    Initial commit

---
+++ src/main/java/com/example/ProjectQ3/ProjectQ3Application.java
@@ -0,0 +1,13 @@
+package com.example.ProjectQ3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectQ3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectQ3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectQ3/ProjectQ3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectQ3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectQ3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
